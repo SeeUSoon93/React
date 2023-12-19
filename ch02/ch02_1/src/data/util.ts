206a4d61d363d05e42043f6560d284d112225c13
김군순 <rlarnstns@gmail.com>
2023-12-19 17:33:29 +0900

    2차할뻔~함

---
+++ ch02/ch02_1/src/data/util.ts
@@ -0,0 +1,6 @@
+// 더미 데이터 만든느 함수
+export const makeArray = (length: number) => new Array(length).fill(null)
+export const range = (min: number, max: number): number[] =>
+  makeArray(max - min).map((notUsed, index) => index + min)
+export const random = (min: number, max: number): number =>
+  Math.floor(Math.random() * (max - min)) + min
